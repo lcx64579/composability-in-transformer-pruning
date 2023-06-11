ac959dbd27b3053471c6b9fb2cfc842b861b90fc
lcx64579 <lcx64579@163.com>
2023-06-10 21:56:39 -0400

    DistilBERT model downloader

--- download_model.py
+++ download_model.py
@@ -1,20 +1,50 @@
 # from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
-from transformers import T5Tokenizer, T5ForConditionalGeneration
-# from transformers.models.t5.modeling_t5 import T5ForConditionalGeneration
 import torch
+import argparse
 
-OUTPUT_DIR = "./model/t5-small"
-OUTPUT_MODEL_PTH = "./model/t5-small.pth"
 
-tokenizer = T5Tokenizer.from_pretrained("t5-small")
-model = T5ForConditionalGeneration.from_pretrained("t5-small")
+# Argument parser
+parser = argparse.ArgumentParser()
+parser.add_argument('model_type', type=str, help='model type')
+args = parser.parse_args()
 
-# model.save_pretrained(OUTPUT_DIR)
-# tokenizer.save_pretrained(OUTPUT_DIR)
-# print(f"Huggingface Model & Tokenizer saved to: {OUTPUT_DIR}")
+assert args.model_type in ['t5', 'distilbert'], "Model type not supported!"
 
-torch.save(model, OUTPUT_MODEL_PTH)
+OUTPUT_MODEL_PTH = ''
+OUTPUT_TOKENIZER_PTH = ''
+model = None
+tokenizer = None
+
+
+if args.model_type == 't5':
+    from transformers import T5Tokenizer, T5ForConditionalGeneration
+    # OUTPUT_DIR = "./model/t5-small"
+    OUTPUT_MODEL_PTH = "./model/t5-small.pth"
+    OUTPUT_TOKENIZER_PTH = "./tokenizer/t5-small.pth"
+
+    model = T5ForConditionalGeneration.from_pretrained("t5-small")
+    tokenizer = T5Tokenizer.from_pretrained("t5-small")
+
+    # model.save_pretrained(OUTPUT_DIR)
+    # tokenizer.save_pretrained(OUTPUT_DIR)
+    # print(f"Huggingface Model & Tokenizer saved to: {OUTPUT_DIR}")
+
+    torch.save(model, OUTPUT_MODEL_PTH)
+    torch.save(tokenizer, OUTPUT_TOKENIZER_PTH)
+
+elif args.model_type == 'distilbert':
+    from transformers import DistilBertTokenizer, DistilBertForSequenceClassification
+
+    OUTPUT_MODEL_PTH = './model/distilbert-imdb.pth'
+    OUTPUT_TOKENIZER_PTH = './tokenizer/distilbert-imdb.pth'
+    model = DistilBertForSequenceClassification.from_pretrained('lvwerra/distilbert-imdb')
+    tokenizer = DistilBertTokenizer.from_pretrained('lvwerra/distilbert-imdb')
+    torch.save(model, OUTPUT_MODEL_PTH)
+    torch.save(tokenizer, OUTPUT_TOKENIZER_PTH)
+
+
+print(f'Model type: {type(model)}')
 print(f".pth Model saved to: {OUTPUT_MODEL_PTH}")
+print(f'.pth Tokenizer saved to: {OUTPUT_TOKENIZER_PTH}')
 
-print(type(model))
 # print(model)

648d79ba972de0cb5e36b0c597c9e0761bc3a4db
lcx64579 <lcx64579@163.com>
2023-06-13 15:20:43 -0400

    Util functions to get num heads and embed size

--- utils.py
+++ utils.py
@@ -30,6 +30,34 @@ def format_time(elapsed):
     return str(datetime.timedelta(seconds=int(round((elapsed)))))
 
 
+def save_checkpoint(model, optimizer, scheduler, this_epoch, loss, training_stats, early_stopping_best_loss, early_stopping_patience_counter, save_path):
+    checkpoint = {
+        'epoch': this_epoch,
+        'model_state_dict': model.state_dict(),
+        'optimizer_state_dict': optimizer.state_dict(),
+        'scheduler_state_dict': scheduler.state_dict(),
+        'loss': loss,
+        'training_stats': training_stats,
+        'early_stopping_best_loss': early_stopping_best_loss,
+        'early_stopping_patience_counter': early_stopping_patience_counter
+    }
+    torch.save(checkpoint, os.path.join(save_path, f'checkpoint_epoch_{this_epoch}.pt'))
+
+
+def load_checkpoint(model, optimizer, scheduler, checkpoint_path):
+    checkpoint = torch.load(checkpoint_path)
+    epoch = checkpoint['epoch']
+    model.load_state_dict(checkpoint['model_state_dict'])
+    optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+    scheduler.load_state_dict(checkpoint['scheduler_state_dict'])
+    loss = checkpoint['loss']
+    training_stats = checkpoint['training_stats']
+    early_stopping_best_loss = checkpoint['early_stopping_best_loss']
+    early_stopping_patience_counter = checkpoint['early_stopping_patience_counter']
+
+    return model, optimizer, scheduler, epoch, loss, training_stats, early_stopping_best_loss, early_stopping_patience_counter
+
+
 # Modify this to add supports to more model types.
 def type_of_model(path_to_model: str) -> str:
     """Return the type of a model based on the path. **This requires the filename
@@ -103,29 +131,39 @@ def type_of_module(model_type: str, name: str, module: nn.Module) -> str:
         return None
 
 
-def save_checkpoint(model, optimizer, scheduler, this_epoch, loss, training_stats, early_stopping_best_loss, early_stopping_patience_counter, save_path):
-    checkpoint = {
-        'epoch': this_epoch,
-        'model_state_dict': model.state_dict(),
-        'optimizer_state_dict': optimizer.state_dict(),
-        'scheduler_state_dict': scheduler.state_dict(),
-        'loss': loss,
-        'training_stats': training_stats,
-        'early_stopping_best_loss': early_stopping_best_loss,
-        'early_stopping_patience_counter': early_stopping_patience_counter
-    }
-    torch.save(checkpoint, os.path.join(save_path, f'checkpoint_epoch_{this_epoch}.pt'))
+# Modify this to add supports to more model types.
+def get_embed_dim(model_type: str, model: nn.Module) -> int:
+    """Get the embedding dimension of a model.
+
+    Args:
+        :param model_type: type of model
+        :param model: model
+
+    Returns:
+        :return: embedding dimension
+    """
+    if model_type == "t5":
+        return model.config.d_model
+    elif model_type == "distilbert":
+        return model.config.dim
+    else:
+        raise NotImplementedError(f"Model type {model_type} is not supported.")
 
 
-def load_checkpoint(model, optimizer, scheduler, checkpoint_path):
-    checkpoint = torch.load(checkpoint_path)
-    epoch = checkpoint['epoch']
-    model.load_state_dict(checkpoint['model_state_dict'])
-    optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
-    scheduler.load_state_dict(checkpoint['scheduler_state_dict'])
-    loss = checkpoint['loss']
-    training_stats = checkpoint['training_stats']
-    early_stopping_best_loss = checkpoint['early_stopping_best_loss']
-    early_stopping_patience_counter = checkpoint['early_stopping_patience_counter']
+# Modify this to add supports to more model types.
+def get_num_heads(model_type: str, model: nn.Module) -> int:
+    """Get the number of heads of a model.
 
-    return model, optimizer, scheduler, epoch, loss, training_stats, early_stopping_best_loss, early_stopping_patience_counter
+    Args:
+        :param model_type: type of model
+        :param model: model
+
+    Returns:
+        :return: number of heads
+    """
+    if model_type == "t5":
+        return model.config.num_heads
+    elif model_type == "distilbert":
+        return model.config.n_heads
+    else:
+        raise NotImplementedError(f"Model type {model_type} is not supported.")

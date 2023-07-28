b031558685a8cb7888c7826f34207f20ab26b9ce
lcx64579 <lcx64579@gmail.com>
2023-07-28 00:01:46 -0400

    Remove buffers first then eval. Also warm up first

--- inference_overhead_evaluate.py
+++ inference_overhead_evaluate.py
@@ -1,4 +1,6 @@
 import torch
+import torch.nn.utils.prune as prune
+from utils import type_of_module, type_of_model
 import numpy as np
 import random
 import os
@@ -15,6 +17,7 @@ assert os.path.exists(PATH_TO_MODEL), "model file not found"
 PATH_TO_TOKENIZER = args.tokenizer
 assert os.path.exists(PATH_TO_TOKENIZER), "tokenizer file not found"
 
+TYPE_OF_MODEL = type_of_model(PATH_TO_MODEL)
 MANUAL_SEED = 42
 
 # Set random seed
@@ -32,16 +35,27 @@ tokenizer = torch.load(PATH_TO_TOKENIZER)
 
 
 # Inference memory overhead
-memory_footprint = model.get_memory_footprint()
+# But first, let's remove all the buffers and apply the pruning mask
+for name, module in model.named_modules():
+    if type_of_module(TYPE_OF_MODEL, name, module) == "Linear" and hasattr(module, 'weight_orig'):
+        prune.remove(module, 'weight')
+        if hasattr(module, 'bias') and module.bias is not None:
+            prune.remove(module, 'bias')
+# Then we calculate the memory footprint
+memory_footprint = model.get_memory_footprint(return_buffers=True)
 print(f'Memory footprint (Bytes): {memory_footprint}')
 
 # Inference time
 sentence = "translate English to German: A group of people are standing in front of an auditorium."
 input_ids = tokenizer(sentence, return_tensors="pt").input_ids.to(device)
 times = []
-for i in range(10):
+# Warm up
+for i in range(100):
+    model.generate(input_ids, max_new_tokens=100)
+# Actual inference
+for i in range(500):
     start = datetime.now()
-    model.generate(input_ids)
+    model.generate(input_ids, max_new_tokens=100)
     end = datetime.now()
     deltatime_by_microseconds = (end - start).microseconds
     times.append(deltatime_by_microseconds)
